5e8b3cbdacf02ab329512036f98331034c64b212
n0bis <madsfalken@Madss-MacBook-Pro.local>
2018-09-26 16:31:51 +0200

    world of zuul init
    world of zuul is out, get it before your mom

---
+++ src/worldofzuul/StartGame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+
+/**
+ *
+ * @author madsfalken
+ */
+public class StartGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var game = new Game();
+        game.play();
+    }
+    
+}
