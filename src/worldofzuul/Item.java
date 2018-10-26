1dca61156f15cda7bdcca108fa5d37661bd9d2c9
Mikkel <42960598+miklmikl@users.noreply.github.com>
2018-10-26 07:25:30 +0200

    Item Class

---
+++ src/worldofzuul/Item.java
@@ -0,0 +1,22 @@
+package worldofzuul;
+
+public class Item {
+    
+    String name;
+    int id;
+    
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
