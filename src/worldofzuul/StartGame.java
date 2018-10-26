92207cd62e4ff7b57423befae9ee046ad65f1866
Mikkel <42960598+miklmikl@users.noreply.github.com>
2018-10-26 07:09:38 +0200

    Changed var to game

--- src/worldofzuul/StartGame.java
+++ src/worldofzuul/StartGame.java
@@ -16,7 +16,7 @@ public class StartGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        var game = new Game();
+        Game game = new Game();
         game.play();
     }
     
