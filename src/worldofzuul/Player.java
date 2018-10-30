184ba1c61986ace061ad419ebc449d6ff473ff3f
Mikkel <42960598+mdhenriksen@users.noreply.github.com>
2018-10-30 06:31:15 -0500

    Feature item (#27)
    * Item Class
    
    * setItem method
    
    Hahaha jeg kan finde ud af at lave commits #wonderchild
    
    * Player-Class
    
    * Error with commands
    
    Commands virker ikke, viser dem ikke/fjerner dem ikke, trods de slettes/tilføjes i CommandWords..
    
    * Commands added (after clean/rebuild)
    
    * Added search method.
    
    Fixed nullpointerexception, player not instantiated
    
    * Item Class
    
    * setItem method
    
    Hahaha jeg kan finde ud af at lave commits #wonderchild
    
    * Player-Class
    
    * Added commandwords
    
    Search and drop
    
    * Added inventory command
    
    * Added getInvnetory method for Player
    
    * Added getItem method for Room
    
    * Added feature - Drop
    
    * Fixed bug drop command
    
    Error in drop command. Would show error message before dropping item you already had.
    
    * Removed logger and level
    
    Mads told me to

---
+++ src/worldofzuul/Player.java
@@ -0,0 +1,26 @@
+package worldofzuul;
+
+import java.util.ArrayList;
+
+public class Player {
+    public ArrayList<Item> inventory = new ArrayList<Item>();
+    
+    public Player() {
+        
+    }
+    
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+    
+    public void getInventory() {
+        if (inventory.isEmpty()) {
+            System.out.println("Too bad. You got no items hombré!");
+        } else {
+            System.out.println("### You inventory is: ###");
+            for(Item item : inventory) {
+                System.out.println("- " + item.name + " ");
+            }
+        }
+    }
+}
