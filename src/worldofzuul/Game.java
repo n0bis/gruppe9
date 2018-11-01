b52d0b8d6812b633c535cba46d74d8db22b18df1
nibas18 <43138810+nibas18@users.noreply.github.com>
2018-11-01 21:23:11 +0100

    Rooms
    Ændringer af rummene

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -16,8 +16,9 @@ public class Game
     private void createRooms()
     {
         Room outsideTek, tekHall, studyRooms, building44lvl1, building44lvl2, building44lvl3, u183, northMainHall,
-            northToilets, u45, u55, southMainHall, building38, u140, building22a, building22aNorth, u27a, building22aSouth, u1,
-                building334, u133, outsideMainHall, nedenunder, theColourKitchen, underTheColourKitchen;
+            northToilets, u45, u55, southMainHall, building38, u140, building22a, building22alvl1, building22aNorth, u27a, building22aSouth, u1,
+                building344, u133, outsideMainHall, nedenunder, theColourKitchen, underTheColourKitchen, library;
+        
       
         outsideTek = new Room("outside the tek entrance of the university");
         tekHall = new Room("inside TEK hall");
@@ -25,15 +26,75 @@ public class Game
         building44lvl1 = new Room("at level 1 in building 44");
         building44lvl2 = new Room("at level 2 in building 44");
         building44lvl3 = new Room("at level 3 in building 44");
+        u183 = new Room("in classroom u183 on level 3 in building 44");
+        northMainHall = new Room("in the northMainHall");
+        northToilets = new Room("in one of the toilets on the northMainHall");
+        u45 = new Room("in classroom u45 which is in the northMainHall");
+        southMainHall = new Room("in the southMainHall");
+        u55 = new Room("in classroom u55 which is in the southMainhall");
+        building38 = new Room("in building38");
+        u140 = new Room("in classroom u140 which is in building38");
+        building22a = new Room("in building22a");
+        building22alvl1 = new Room("at level 1 in building22a, but in building22a there are 3 levels");
+        building22aNorth = new Room("in building22aNorth");
+        u27a = new Room("in classroom 22a which is in building22asouthlvl1");
+        building22aSouth = new Room("in building22aSouth");
+        u1 = new Room("in classroom u1 which is in building22a");
+        building344 = new Room("in building344");
+        u133 = new Room("in classroom u133 which is in building344");
+        outsideMainHall = new Room("outside of the main hall");
+        nedenunder = new Room("in fredagsbaren where the halloween party is being held");
+        theColourKitchen = new Room("in the biggest cafeteria of the southern university");
+        underTheColourKitchen = new Room("under the biggest cafeteria of the southern university");
+        library = new Room("the library of the university");
+ 
+        currentRoom = outsideTek;
         
         outsideTek.setExit("north", tekHall);
         tekHall.setExit("up", studyRooms);
-        
         studyRooms.setExit("down", tekHall);
+        tekHall.setExit("east",building44lvl1);
+        building44lvl1.setExit("up",building44lvl2);
+        building44lvl2.setExit("up",building44lvl3);
+        building44lvl3.setExit("to u183", u183); 
+        u183.setExit("back", building44lvl3); 
+        building44lvl3.setExit("down",building44lvl2);
+        building44lvl2.setExit("down",building44lvl1);
+        building44lvl1.setExit("east",northMainHall);
+        northMainHall.setExit("down",northToilets);
+        northToilets.setExit("up",northMainHall);
+        northMainHall.setExit("to u45", u45);
+        u45.setExit("back",northMainHall);
+        northMainHall.setExit("to u55",u55);
+        u55.setExit("back", northMainHall);
+        northMainHall.setExit("south",southMainHall);
+        southMainHall.setExit("to TheColourKitchen", theColourKitchen);
+        theColourKitchen.setExit("to nedenunder",nedenunder);
+        nedenunder.setExit("back",theColourKitchen);
+        theColourKitchen.setExit("back", southMainHall);
+        southMainHall.setExit("east",building344);
+        building344.setExit("to u133",u133);
+        u133.setExit("back",building344);
+        building344.setExit("back",southMainHall);
+        southMainHall.setExit("to library",library);
+        library.setExit("back", southMainHall);
+        southMainHall.setExit("north",northMainHall);
+        northMainHall.setExit("west",building38);
+        building38.setExit("to u140", u140);
+        u140.setExit("back", building38);
+        building38.setExit("back",northMainHall);
+        northMainHall.setExit("to building22a",building22a);
+        building22a.setExit("to building22aSouth",building22aSouth);
+        building22a.setExit("to u1",u1);
+        u1.setExit("back",building22aSouth);
+        building22aSouth.setExit("back",building22a);
+        building22a.setExit("to building22aNorth", building22aNorth);
+        building22aNorth.setExit("up",u27a);
+        u27a.setExit("back",building22aNorth);
+        building22aNorth.setExit("back", building22a);   
         
-        currentRoom = outsideTek;
     }
-
+    
     public void play() 
     {            
         printWelcome();
