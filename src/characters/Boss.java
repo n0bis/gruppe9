feb10bcfff88d68bce0cca339524c698d701b784
n0bis <madsfalken@Madss-MacBook-Pro.local>
2018-11-14 15:44:48 +0100

    move comment to the right place

--- src/characters/Boss.java
+++ src/characters/Boss.java
@@ -2,13 +2,6 @@ package characters;
 
 
 public class Boss {
-    /* Boss concept:
-    - Boss has 3 quests that needs to be completed in order to kill him
-    - If you do not complete the 3 things, you will be thrown back
-    - One command will be a spell
-    
-    Problem: go commands will still show?
-    */
     private String name;
     private String dialogue;
     
