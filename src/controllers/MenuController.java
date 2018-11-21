3ebe77a5ee8ad7a98164614f9f4f70fdce5deee8
n0bis <madsfalken@Madss-MacBook-Pro.local>
2018-11-21 22:09:38 +0100

    cast fireball - javafx makes no sense

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -8,6 +8,7 @@ package controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,9 +18,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import static utils.ShowMap.showMap;
 import static world.Game.fireball;
 import static world.Game.player;
@@ -40,6 +46,8 @@ public class MenuController implements Initializable {
     private TextArea SpeechId;
     @FXML
     private ImageView spellBookId;
+    @FXML
+    private AnchorPane rootId;
 
     /**
      * Initializes the controller class.
@@ -82,6 +90,24 @@ public class MenuController implements Initializable {
             fireBallIcon.setOnMouseClicked((value) -> {
                 System.out.println("imma firin mah lazer");
                 dialog.close();
+                
+                BorderPane main = SceneManager.getMain();
+                
+                Circle cir = new Circle();
+                cir.setFill(Color.FIREBRICK);
+                cir.setRadius(30);
+                cir.setLayoutX(main.getBoundsInLocal().getMaxX() / 2);
+                cir.setLayoutY(800);
+                cir.setId("fireball");
+                cir.toFront();
+                
+                TranslateTransition transition = new TranslateTransition();
+                transition.setDuration(Duration.seconds(3));
+                transition.setToY(-(main.getBoundsInLocal().getMaxY() / 2));
+                transition.setNode(cir);
+                transition.play();
+                transition.setOnFinished((val) -> main.getChildren().remove(cir));
+                main.getChildren().add(cir);
             });
         }
         dialogVbox.getChildren().add(root);
