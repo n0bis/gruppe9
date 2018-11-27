5a89af893aec201b47d1eea7d25ffa34f37beb12
n0bis <supermads@hotmail.com>
2018-11-27 12:19:57 +0100

    time remaining and time expired popup/dialog windows (#62)

--- src/utils/TimeExpired.java
+++ src/utils/TimeExpired.java
@@ -6,6 +6,15 @@
 package utils;
 
 import java.util.TimerTask;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -14,7 +23,22 @@ import java.util.TimerTask;
 public class TimeExpired extends TimerTask {
     @Override
     public void run() {
-        System.out.println("Time expired - You lost");
-        System.exit(0);
+        Platform.runLater(() -> {
+            final Stage dialog = new Stage();
+            dialog.initModality(Modality.APPLICATION_MODAL);
+            VBox dialogVbox = new VBox(20);
+            Button okButton = new Button("Close");
+            okButton.setOnAction((value) -> System.exit(0));
+            Text loserText = new Text("You lost the game, you'll forever be lost in the SDU Maze and you'll never get to met the hot nurse");
+            Text score = new Text("Thank you for playing SCORE: " + (System.currentTimeMillis() - TimerScore.getTime()) / 1000L);
+            dialogVbox.getChildren().addAll(loserText, score);
+            Scene dialogSence = new Scene(dialogVbox);
+            dialog.setScene(dialogSence);
+            dialog.setAlwaysOnTop(true);
+            dialog.setResizable(false);
+            dialog.show();
+            dialog.setOnCloseRequest((value) -> System.exit(0));
+            dialog.setOnHiding((value) -> System.exit(0));
+        });
     }
 }
