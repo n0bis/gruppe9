aa7bb8e40d6fe2fde15f757279bbda93f2f4ffc7
n0bis <madsfalken@Madss-MacBook-Pro.local>
2018-12-03 09:56:07 +0100

    remove unused imports

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -6,38 +6,20 @@
 package controllers;
 
 import items.SpellBook;
-import utils.SpriteAnimation;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.animation.Animation;
-import javafx.animation.TranslateTransition;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import missions.Quest;
-import static world.Game.fireball;
 import static world.Game.player;
-import static world.Game.spellBook;
 
 /**
  * FXML Controller class
