d4affc85dbf98e672b8d3d55f559c25bad0098e2
n0bis <supermads@hotmail.com>
2018-12-04 11:22:51 +0100

    Feature enter game (#81)
    * Preview of start-game screen
    
    * Test commit til Mads
    
    * Enter-game done - Fixed previous errors 
    
    Enter game done with Start Game button etc. Opening and adding menu controller to following Stage without having it on the start-screen.
    
    * Chose option to start -> Changes toiletpaper in first scene
    
    An example implementation if chose "hat". Radiobutton can be swapped with image, or images can be places above. Depends on what we want.
    
    * Setting player name + receiving welcome message with name

--- src/worldofzuul/StartGame.java
+++ src/worldofzuul/StartGame.java
@@ -15,7 +15,10 @@ import java.util.concurrent.TimeUnit;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import utils.TimeExpired;
@@ -30,9 +33,38 @@ import world.Game;
 public class StartGame extends Application {
     
     public static Game game = new Game();
+    public static Stage primaryStage = new Stage();
+    public static String imageSelector;
+    public static String playerName;
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage startStage) throws IOException {
+        this.primaryStage = startStage;
+        Parent root = FXMLLoader.load(getClass().getResource("/views/StartGameFXML.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("SDU Maze");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }      
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        StartGame.playerName = playerName;
+    }
+    
+    public String getImageSelector() {
+        return imageSelector;
+    }
+
+    public void setImageSelector(String imageSelector) {
+        this.imageSelector = imageSelector;
+    }
+    
+    
+    public void second() {
         BorderPane borderPane = new BorderPane();
         FXMLLoader loader = new FXMLLoader(
             getClass().getResource("/views/Menu.fxml")
@@ -46,7 +78,7 @@ public class StartGame extends Application {
         MenuController menuController = loader.<MenuController>getController();
         
         Scene scene = new Scene(borderPane);
-        
+    
         scene.getStylesheets().add("/styles/base.css");
         
         SceneManager sceneManager = new SceneManager(borderPane);
@@ -94,27 +126,20 @@ public class StartGame extends Application {
         sceneManager.addScene("stairsTowardsNedenUnder", menuController);
         sceneManager.addScene("nedenUnder", menuController);
         sceneManager.addScene("nedenUnderToilets", menuController);
-        
-        
-                
                 
         Timer timer = new Timer();
         timer.schedule(new TimeRemaining(), TimeUnit.MINUTES.toMillis(10));
         timer.schedule(new TimeExpired(), TimeUnit.MINUTES.toMillis(20));
         TimerScore.startTimer();
         
-        SceneManager.activate("torvet");
+        SceneManager.activate("outsideTekFar");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
         primaryStage.setOnCloseRequest((value) -> timer.cancel());
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     
+    public static void main(String[] args) throws IOException {
         launch(args);
     }
-    
 }
