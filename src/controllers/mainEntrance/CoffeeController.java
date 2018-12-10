b4c9a7f5adc45c6365e70edc3c309aa1676b4f5b
nibas18 <43138810+nibas18@users.noreply.github.com>
2018-12-10 09:44:05 +0100

    NowTheCoffeeMachineIsOfficiallyDone
    Now you have to get the coin before you can get a coffee and if you get the coin and buy yourself coffee then the coin disappears from you inventory (sack)

--- src/controllers/mainEntrance/CoffeeController.java
+++ src/controllers/mainEntrance/CoffeeController.java
@@ -67,10 +67,13 @@ public class CoffeeController extends MenuControllerInjection implements Initial
         if (player.hasItem(coin)) {
             coffeeDrinkId.setImage(null);
             player.addItem(coffeeDrink);
-            // remove coin again ?
+            // remove coin
             menuController.SpeechText("You've had to much coffee therefore you need to find the toilet which is downstairs");
         } else {
             menuController.SpeechText("You need a coin to buy coffee");
+        }
+        if (player.getInventory().contains(coffeeDrink)) {
+            player.getInventory().remove(coin); 
         }
     }
 }
