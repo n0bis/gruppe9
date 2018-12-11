b21484bfe7a509257c3b7d319c60cc4fe0cb4db7
nibas18 <43138810+nibas18@users.noreply.github.com>
2018-12-11 13:11:33 +0100

    Update QuestList.java

--- src/missions/QuestList.java
+++ src/missions/QuestList.java
@@ -18,7 +18,7 @@ public class QuestList {
     public static Quest sackQuest = new Quest("Find my book", "This is the quest description: Find my book", "Quest is in progress: You still need to find my book", "The quest is "
             + "done", teeth, Bookie);    
     
-    public static Quest Twin1Quest = new Quest("Find my bear bear!", "I've lost my bear... If you find my bear I'll let you pass. I'll give you a hint, walk past krogene", "You still need to find my bear!", "The quest is "
+    public static Quest Twin1Quest = new Quest("Find my bear!", "I've lost my bear... If you find my bear I'll let you pass. I'll give you a hint, walk past krogene", "You still need to find my bear!", "The quest is "
             + "done", null, creepyBear);
     public static Quest Twin2Quest = new Quest("Find my doll", "I've lost my doll... If you find my doll I'll let you pass. I'll give you a hint, you have to go through vangene", "You still need to find my doll!", "The quest is "
             + "done", null, creepyDoll);
