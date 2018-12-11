9b5e8ec45df2d38e80f3a209884df40b6603a071
nibas18 <43138810+nibas18@users.noreply.github.com>
2018-12-11 12:34:41 +0100

    Coffee machine (#93)
    * CoffeeMachine
    
    * CoffeeMachine
    
    CoffeeDrink and studyCard is now in the inventory bag but since I can't open the coffee.FXML I can't put the coffee in atm
    
    * CoffeeMachine
    
    You can now click on the coffe but it wont be visible in your inventory bag which is weird
    
    * TryingToMakeACountdown
    
    * now it shows how many clicks you've left
    
    * Update CoffeeController.java
    
    * NowTheCoffeeMachineWorks
    
    * TheCoffeeMachineIsOfficiallyDone
    
    * b
    
    * f
    
    * remove spaces from item name and display coffee in bag
    
    * check if player has the golden coin or else no coffee for you
    
    * NowTheCoffeeMachineIsOfficiallyDone
    
    Now you have to get the coin before you can get a coffee and if you get the coin and buy yourself coffee then the coin disappears from you inventory (sack)

--- src/controllers/GydehuttenNorth/EntranceToTorvetController.java
+++ src/controllers/GydehuttenNorth/EntranceToTorvetController.java
@@ -10,10 +10,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import utils.FadeAnimation;
+import static world.Game.coin;
+import static world.Game.player;
 
 /**
  * FXML Controller class
@@ -30,6 +33,8 @@ public class EntranceToTorvetController extends MenuControllerInjection implemen
     private ImageView arrowForwardId;
     @FXML
     private ImageView arrowBackId;
+    @FXML
+    private ImageView coinId;
 
     /**
      * Initializes the controller class.
@@ -38,6 +43,7 @@ public class EntranceToTorvetController extends MenuControllerInjection implemen
     public void initialize(URL url, ResourceBundle rb) {
         backgroundId.fitHeightProperty().bind(anchorId.heightProperty());
         backgroundId.fitWidthProperty().bind(anchorId.widthProperty());
+        coinId.setImage(new Image(getClass().getResourceAsStream("/images/items/Coin.png")));
         // TODO
     }    
 
@@ -50,5 +56,13 @@ public class EntranceToTorvetController extends MenuControllerInjection implemen
     private void arrowBackClicked(MouseEvent event) {
         FadeAnimation.fadeOutTransition(anchorId, "mainhall5");
     }
-    
-}
+    @FXML
+    private void coinClicked(MouseEvent event) {
+        coinId.setImage(null);
+        player.addItem(coin); 
+        if (player.hasItem(coin)) {
+            menuController.SpeechText("You can now buy yourself a coffee from the vending machine down the corridor");    
+        }
+        }
+    }
+
