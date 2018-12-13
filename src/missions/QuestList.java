0d7f26ffff58a32c812ec692dd89da9fc06b7f43
Mathiask1 <birkebjerg99@live.dk>
2018-12-13 17:39:44 +0100

    Removed unsued code

--- src/missions/QuestList.java
+++ src/missions/QuestList.java
@@ -20,13 +20,16 @@ import static world.Game.toiletpaper;
  */
 public class QuestList {
     public static Quest sackQuest = new Quest("Find my book", "This is the quest description: Find my book", "Quest is in progress: You still need to find my book", "The quest is "
-            + "done", teeth, Bookie);    
+            + "done", teeth, Bookie);    // TEST QUEST
     
     public static Quest Twin1Quest = new Quest("Find my bear!", "I've lost my bear... If you find my bear I'll let you pass. I'll give you a hint, walk past krogene", "You still need to find my bear!", "The quest is "
             + "done", null, creepyBear);
+    
     public static Quest Twin2Quest = new Quest("Find my doll", "I've lost my doll... If you find my doll I'll let you pass. I'll give you a hint, you have to go through vangene", "You still need to find my doll!", "The quest is "
             + "done", null, creepyDoll);
-public static Quest doctorQuest = new Quest("Find my bones", "I need you to find 3 bones", "You still need to find all 3 bones", "Uhh, thanks for the help boy. I've unlocked the hallways again", boneone, bonetwo, bonethree);
+    
+    public static Quest doctorQuest = new Quest("Find my bones", "I need you to find 3 bones", "You still need to find all 3 bones", "Uhh, thanks for the help boy. I've unlocked the hallways again", boneone, bonetwo, bonethree);
+    
     public static Quest mummyQuest = new Quest("Find me some toiletpaper", "I need you to find me some toiletpaper because this is getting too old", "You still need to get me toiletpaper", "Thank you! Now I feel so young. I've unlocked the hallways again", toiletpaper);
 
 }
