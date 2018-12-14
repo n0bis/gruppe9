64fe02f5f118645c0c906dcc1c133559af706ce4
Mathiask1 <birkebjerg99@live.dk>
2018-12-14 08:57:27 +0100

    optimisation

--- src/missions/QuestList.java
+++ src/missions/QuestList.java
@@ -19,9 +19,6 @@ import static world.Game.toiletpaper;
  * @author birke
  */
 public class QuestList {
-    public static Quest sackQuest = new Quest("Find my book", "This is the quest description: Find my book", "Quest is in progress: You still need to find my book", "The quest is "
-            + "done", teeth, Bookie);    // TEST QUEST
-    
     public static Quest Twin1Quest = new Quest("Find my bear!", "I've lost my bear... If you find my bear I'll let you pass. I'll give you a hint, walk past krogene", "You still need to find my bear!", "The quest is "
             + "done", null, creepyBear);
     
