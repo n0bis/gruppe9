0689af521342f29345b227cd5c74e210f26610c8
Mathiask1 <birkebjerg99@live.dk>
2018-12-14 09:43:20 +0100

    Feature help button (#138)
    * Added help button and removed white border from spellbook
    
    * changes
    
    * RettetHelp
    
    * Update Help.fxml

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -107,6 +107,19 @@ public class MenuController implements Initializable {
         root.setLayoutX(200);
         root.setLayoutY(60);
         SceneManager.getMain().getChildren().add(root);
+    }    
+    @FXML
+    private void helpClicked(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+           getClass().getResource("/views/Help.FXML")
+        );
+        Parent root = loader.load();
+      
+        Group closeButton = (Group)loader.getNamespace().get("crossId");
+        closeButton.setOnMouseClicked((mouseEvent) -> SceneManager.getMain().getChildren().remove(root));
+        root.setLayoutX(200);
+        root.setLayoutY(60);
+        SceneManager.getMain().getChildren().add(root);
     } 
     
     @FXML
