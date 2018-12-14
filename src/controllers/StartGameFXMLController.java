6719f6f0f3985b8e18537382f27c0653f237a093
n0bis <madsfalken@Madss-MacBook-Pro.local>
2018-12-14 10:58:43 +0100

    add le scissor

--- src/controllers/StartGameFXMLController.java
+++ src/controllers/StartGameFXMLController.java
@@ -76,7 +76,7 @@ public class StartGameFXMLController implements Initializable {
             menuControler.stopSound();
             startGame.setPlayerName(enterNameTextField.getText());
             startGame.primaryStage.close();
-            startGame.third();
+            startGame.second();
             
         } else {
             labelMessage.setOpacity(1);
