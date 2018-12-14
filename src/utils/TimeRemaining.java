7049b64785e4c7c59ede482683b83de97ca24d33
Mathiask1 <birkebjerg99@live.dk>
2018-12-14 10:54:49 +0100

    Feature new sound4 reminder message (#144)
    * Added reminder.mp3
    
    * Update TimeRemaining.java
    
    * play sound
    
    * Update TimeRemaining.fxml

--- src/utils/TimeRemaining.java
+++ src/utils/TimeRemaining.java
@@ -7,9 +7,12 @@ package utils;
 
 import controllers.SceneManager;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.TimerTask;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -20,6 +23,8 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import static world.Game.player;
 import worldofzuul.StartGame;
 
@@ -27,8 +32,17 @@ import worldofzuul.StartGame;
  *
  * @author birke
  */
-public class TimeRemaining extends TimerTask  {
-
+public class TimeRemaining extends TimerTask implements Initializable {
+    
+    private MediaPlayer mediaPlayer;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mediaPlayer = new MediaPlayer(new Media(getClass().getResource("/sounds/Reminder.mp3").toExternalForm()));
+        mediaPlayer.setVolume(1);
+        mediaPlayer.play();
+    }
+    
     @Override
     public void run() {
         Platform.runLater(() -> {
@@ -40,13 +54,14 @@ public class TimeRemaining extends TimerTask  {
                 Button closeButton = (Button)loader.getNamespace().get("closeButton");
                 closeButton.setOnMouseClicked((mouseEvent) -> SceneManager.getMain().getChildren().remove(root));
                 FlowPane flowPane = (FlowPane)loader.getNamespace().get("flowPane");
-                flowPane.setBackground(new Background(new BackgroundImage(new Image(getClass().getResourceAsStream("/images/smartphone.png")), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+                flowPane.setBackground(new Background(new BackgroundImage(new Image(getClass().getResourceAsStream("/images/menu/smartphone.png")), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
                 TextArea smsTextbox = (TextArea)loader.getNamespace().get("smsTextbox");
                 String text = smsTextbox.getText().replaceFirst("Player", StartGame.playerName);
                 smsTextbox.setText(text);
                 root.setLayoutX(300);
                 root.setLayoutY(60);
                 SceneManager.getMain().getChildren().add(root);
+
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
